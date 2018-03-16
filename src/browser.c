e53c9c6bea60fde3ddad26353fe4ec672d6d337f
blmayer <bleemayer@gmail.com>
2018-03-16 03:16:23 +0000

    Renamed main file accordingly.

--- src/browser.c
+++ src/browser.c
@@ -8,8 +8,8 @@
  * FUNCTION:    This is the main file, here we manage to open TCP connections
  *              in order to connect to an URL. Then each connection is proces -
  *              sed by a different thread, created by a fork command, that will
- *              write the request, wail for a responsen interpret it, and gene-
- *              rate the page.
+ *              write the request, wail for a response and interpret it, and 
+ *              generate the page.
  *
  * AUTHOR:      Brian Mayer blmayer@icloud.com
  *
@@ -26,16 +26,16 @@
 #include <sys/wait.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include "receivefn.h"
+#include "connect.h"
 
 int main(void){
 
     /* Initiate a TCP socket */
-    int socket, conn;
+    int conn_socket;
 
     /* Create the socket */
-    socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket == -1){
+    conn_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(conn_socket == -1){
         perror("Could not create socket");
     }
     puts("Socket created");
@@ -47,36 +47,46 @@ int main(void){
     server.sin_port = htons(8888);
 
     /* Connect to remote server */
-    conn = connect(socket, (struct sockaddr *)&server, sizeof(server));
-
-    if(conn < 0){
+    if(connect(conn_socket, (struct sockaddr *)&server, sizeof(server)) < 0){
         perror("Connect failed");
         return -1;
     }
     puts("Connected\n");
 
-	/* data that will be sent to the server */
-	const char* data_to_send = "Gangadhar Hi Shaktimaan hai";
-	send(sock, data_to_send, strlen(data_to_send), 0);
+	/* Send a GET request to the server */
+	unsigned char* request;
+    
+	send(conn_socket, data_to_send, strlen(data_to_send), 0);
 
-	// receive
+    /* Initialize variables for reading the request */
+    puts("\tReady to receive.");
+    int pos = 0, buff_size = 1024;
+    unsigned char *buffer = calloc(buff_size, 1);
+    unsigned char *dec_buffer = calloc(buff_size, 1);
 
-	int n = 0;
-	int len = 0, maxlen = 100;
-	char buffer[maxlen];
-	char* pbuffer = buffer;
+    /* This is a loop that will read the data comming from our connection */
+    while(read(conn_socket, buffer + pos, 1) == 1){
+        
+        /* Decode while we read */
+        dec_buffer[pos] = (buffer[pos] - 1) ^ KEY[pos % 512];
+        
+        /* Increase pos by 1 to follow the buffer size */
+        pos++;
+        
+        /* The only thing that can break our loop is a blank line */
+        if(strcmp(buffer + pos - 4, "\r\n\r\n") == 0){ 
+            break;
+        }
+        
+        if(pos == buff_size){
+            puts("\t\tBuffer grew.");
+            buff_size += 512;
+            buffer = realloc(buffer, buff_size);
+            dec_buffer = realloc(dec_buffer, buff_size);
+        }
+    }
 
-	// will remain open until the server terminates the connection
-	while ((n = recv(sock, pbuffer, maxlen, 0)) > 0) {
-		pbuffer += n;
-		maxlen -= n;
-		len += n;
-
-		buffer[len] = '\0';
-		printf("received: '%s'\n", buffer);
-	}
-
-	close(socket);
+	close(conn_socket);
     puts("End of program.");
     return 0;
 }
