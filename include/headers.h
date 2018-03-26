3351aea91b38178512b30f9fccc341a174e123e3
Brian Mayer <bleemayer@gmail.com>
2018-03-25 23:07:28 -0300

    Removed key from file.

--- include/headers.h
+++ include/headers.h
@@ -8,15 +8,6 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
-#define KEY "Ie70tGt3A2TYntzomlrdQqxlZjWm92AcRKcoOLflbMckfnN9oLUqK9zI0y4wDyJk"\
-            "RFX4ECITyLGfnO2ZwVd0hcp0Yel6F6fuFIaoOIomyW3AXEICZWzV7br9eghUsCHx"\
-            "JRd3s1G0Y9UCwgiMIZTIqeLLRIplgk1WSnwtvcGEE48CHMnubye4TKbAMevftjfA"\
-            "3BDuIlc2Ka4LK67ZCWwaftAbRBEOBz6uHAhy0qSf9PIeSFZITm5biznN3PaolkuP"\
-            "nHUcbsng9Wvp7K2QMTL3wOceBvmwXOvRONReyShCMEMkytRj1HC08683oudaPS5T"\
-            "fBrRG1kpt9ksUn2fpmlqXAyr2EdPTHd8FqO9WWnFPtnSDQbzrtpRoso3MCiJZKKO"\
-            "Tlz6KyzMXwB2IdAQdXlTuxbhWXV0MlDoTxpsnxZLXAiyBmDd2LKTaJ1e8NKLW5sE"\
-            "rdsjcmKgKUYIK7qoyq1DDiCrSVcDbWFrBrnTqrUOTNaQGk7GpwiTEIwtSNAecEDB"
-
 /* Our structure that contains the response's data */
 struct response {
     int status;
@@ -47,4 +38,3 @@ struct request {
 };
 
 #endif
-
