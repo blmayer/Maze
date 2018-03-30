c3d69e4c65f18622c022c8b311213b3716f6a5ff
blmayer <bleemayer@gmail.com>
2018-03-30 21:15:33 +0000

    Created link to the corrected file.

--- include/key.h
+++ include/key.h
@@ -1,10 +1,1 @@
-/*
- *  In this file we place the most important part of the project: the key.
- */
-
-#ifndef KEY_H
-#define KEY_H
-#define KEY "blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#"
-
-#endif
-
+../../Randomator/include/key.h
