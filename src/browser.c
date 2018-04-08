d0f92507b3022e8e5b7c6764de4956d563377f7f
blmayer <bleemayer@gmail.com>
2018-04-08 23:19:41 +0000

    Corrected url parsing, added chunked data read.

--- src/browser.c
+++ src/browser.c
@@ -46,45 +46,65 @@ int main(int argc, char *argv[]){
 	/* ---- Parse the url entered to get host, path and parameters --------- */
 
 	char *proto, *domain, *path, *pars;
-	printf("arg: %s\n", argv[1]);
 	
-	/* Try to match cases http://domain... and //domain... in URL */
-	proto = strstr(argv[1], "//");
-	if(proto == NULL){
-		/* URL is like www.domain... or /file..., get everything */
-		domain = strtok(argv[1], "/");
-		path = strtok(NULL, "");
-	} else {
-		/* Advance to next / to get the domain */
-		domain = strtok(proto + 2, "/");
-		path = strtok(NULL, ""); 				/* Get the path after the / */
+	/* First five bytes gives you the protocol */
+	switch(strncmp(argv[1], "http:", 5)){
+	case 0:
+		proto = "http";
+		break;
+
+	case 57:
+		proto = "https";
+		break;
+	
+	default:
+		proto = NULL;
 	}
 
+	/* TODO Pass only one time over the url string */
+	/* Now we find the domain */
+	if(proto == NULL){
+		/* URL is like www.domain... or /file..., get everything */
+		strncpy(domain, argv[1], strcspn(argv[1], "/"));
+	} else {
+		/* Try to match cases http://domain... and //domain... in URL */
+		domain = strtok(argv[1] + strlen(proto) + 3, "/");
+	}
+	
 	/* Check if domain is not null */
 	if(domain == NULL){
 		puts("Could not find a domain to lookup, please enter a domain.");
 		return -1;
 	}
+	
+	/* Now get the path */
+	path = strtok(NULL, "");
+	if(path == NULL){
+		path = "/";
+		pars = NULL;
+	} else {
+		pars = strchr(path, '?'); 		/* It is easier to find pars first */
+		path = strchr(path, '/'); 		/* Get the path after the / */
+		if(path == NULL){
+			path = "/";
+		}
+	}
 
 	printf("proto is: %s\n", proto);
 	printf("domain is: %s\n", domain);
 	printf("path is: %s\n", path);
-
-	/* To get parameters look for a ? */
-	if(path == NULL){
-		path = "/";
-	}
+	printf("pars is: %s\n", pars);
 
 	/* Lookup host name */
 	struct addrinfo hints = {0};
-	struct addrinfo *server;
+	struct addrinfo *host;
 
 	hints.ai_family = AF_INET;			/* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_STREAM; 	/* Datagram socket */
+	hints.ai_socktype = SOCK_STREAM; 	/* TCP socket */
 	hints.ai_flags = 0;					/* For wildcard IP address */
 	hints.ai_protocol = 0; 				/* Any protocol */
 
-	int lookup = getaddrinfo(domain, argv[2], &hints, &server);
+	int lookup = getaddrinfo(domain, argv[2], &hints, &host);
 	if(lookup != 0){
 		/* Get the host info */
 		puts("Unable to resolve host.");
@@ -92,24 +112,27 @@ int main(int argc, char *argv[]){
 	}
 
 	/* Open a socket */
-	int tcp_server = socket(AF_INET, SOCK_STREAM, 0);
-	if(tcp_server < 0){
+	int server = socket(AF_INET, SOCK_STREAM, 0);
+	if(server < 0){
 		perror("Socket creation failed");
 		return 0;
 	}
 
 	/* Loop on the returned result checking for an address */
-	while(connect(tcp_server, server -> ai_addr, server -> ai_addrlen) == -1){
-		server = server -> ai_next; 
+	while(connect(server, host -> ai_addr, host -> ai_addrlen) == -1){
+		host = host -> ai_next; 
 	}
 	
-	if(server == NULL){
+	if(host == NULL){
 		puts("Could not connect.");
 		return -1;
 	}
-
 	puts("Connection successful!");
 	
+	/* Set the socket timeout */
+	struct timeval timeout = {10, 0};   /* Timeout structure: 10 seconds */
+	setsockopt(server, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, 18);
+
 	/* Create a request structure */
 	struct request req = {0};
 	req.type = "GET";
@@ -117,39 +140,67 @@ int main(int argc, char *argv[]){
 	req.vers = 1.1;
 	req.conn = "Keep-Alive";
 
-	if(server -> ai_canonname == NULL){
+	if(host -> ai_canonname == NULL){
 		req.host = domain;
 	} else {
-		req.host = server -> ai_canonname;
+		req.host = host -> ai_canonname;
 	}
 	
 	/* Send a not yet encrypted GET request */
-	send_get(tcp_server, req);
+	send_get(server, req);
 	
-	unsigned char *response = get_header(tcp_server);
-
-	puts(response);
-
 	/* Populate the response struct */
+	unsigned char *response = get_header(server);
 	struct response res = {0};
 	parse_response(response, &res);
 
-	/* Read response's body */
-	if(res.clen == 0){
-		puts("No body to read, quiting...");
-		close(tcp_server);
-		return 0;
-	}
+	/* ---- Read response's body ------------------------------------------- */
 
 	/* Allocate space for the body */
-	char body[res.clen + 1];
+	unsigned char *body = NULL;
 
-	/* Read the content from the socket */
-	read(tcp_server, body, res.clen);
+	/* The transfer may be normal */
+	if(res.clen > 0){
+		/* Allocate the correct size */
+		body = realloc(body, res.clen + 1);
+		
+		/* Read the content from the socket */
+		read(server, body, res.clen);
+	}
 
-	body[res.clen] = 0;
+	/* Or may be chunked */
+	if(strcmp(res.ttype, "chunked") == 0){
+		/* Here we read and update the body */
+		unsigned char *chunk;
+		int chunk_size; 				/* The size to be read */
+		int body_size = 0; 				/* This space is for the end 0 */
+	
+get_chunk:
+		puts("\tReceiving chunked data...");
+		chunk = read_chunk(server);
+		sscanf(chunk, "%x", &chunk_size); 
+		
+		if(chunk_size > 0){
+			/* Allocate the size needed and read */
+			body = realloc(body, body_size + chunk_size);
+			
+			/* Now read the whole chunk, be sure */
+			int got = 0;
+			while(got < chunk_size){
+				got += read(server, body + body_size + got, chunk_size - got);
+			}
+			
+			body_size += chunk_size;
+			read(server, chunk, 2); 	/* This will discard a \r\n */
+			goto get_chunk;
+		}
+	}
 
 	printf("Body:\n%s\n", body);
+	free(body);
+
+	/* Body read, close the connection */
+	close(server);
 
 	return 0;
 }
