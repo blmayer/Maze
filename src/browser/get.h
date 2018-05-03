375574c68106fcf669c9a158b76becaf24dbcb3e
blmayer <bleemayer@gmail.com>
2018-05-03 03:52:21 +0000

    Added maze browser sources and makefile template.

---
+++ src/browser/get.h
@@ -0,0 +1,19 @@
+/*
+ *  This is the function that sends a GET request of a file, it takes
+ *  two arguments: the client's connection and the file requested in the GET
+ *  message.
+ */
+
+#ifndef GETFN_H
+#define GETFN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <web.h>
+
+/* Receives client's parameters and sends a file */
+int send_get(int conn, struct request req);
+
+#endif
+
