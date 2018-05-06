6d31fe9c75e1793ea7c064bc4e170936e64c1013
blmayer <bleemayer@gmail.com>
2018-05-06 18:28:16 +0000

    Corrected user agent string.

--- src/maze/get.c
+++ src/maze/get.c
@@ -22,26 +22,26 @@
 
 int send_get(int conn, struct request req){
 
-    /* Verify the connection and request version */
-    if(req.conn == NULL && req.vers > 1){
-    	req.conn = "Keep-Alive";
-    }
+	/* Verify the connection and request version */
+	if(req.conn == NULL && req.vers > 1){
+		req.conn = "Keep-Alive";
+	}
 
 	/* Client identification */
-    req.user = "Maze/<VERSION> (x86_64)";
+	req.user = "Maze-" VERSION;
 	
 	/* Accept encoding */
 	req.cenc =  "*/*";
 
-    /* Create the head */
-    unsigned char *request = create_req_header(req);
+	/* Create the head */
+	unsigned char *request = create_req_header(req);
 
 	printf("request:\n%s\n", request);
 
-    /* Send the request */
-    write(conn, request, strlen(request));
-    
-    puts("\tGET request sent.");
+	/* Send the request */
+	write(conn, request, strlen(request));
+
+	puts("\tGET request sent.");
     
 	return 0;
 }
