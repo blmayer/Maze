16cdc5345e574f2ac415d7f0ddf53c8aee9c1b9f
blmayer <bleemayer@gmail.com>
2018-06-14 03:25:56 +0000

    Organized the code.

--- tests/parse_URL.c
+++ tests/parse_URL.c
@@ -1,11 +1,10 @@
 /* Test file for the parse_URL function */
 #include <stdio.h>
-#include <webng.h>
+#include "webng.h"
 
 int main(int argc, char *argv[])
 {
-	if(argc > 2)
-	{
+	if(argc > 2) {
 		puts("Please supply exactly one argument");
 		return 1;
 	}
@@ -13,8 +12,18 @@ int main(int argc, char *argv[])
 	struct url addr = {0};
 	parse_URL(argv[1], &addr);
 
-	printf("parsed:\nproto: %s\ndomain: %s\nport: %s\npath: %s\npars: %s\n",
-		addr.proto, addr.domain, addr.port, addr.path, addr.pars);
+	printf(
+		"proto: %s\n"
+		"domain: %s\n"
+		"port: %s\n"
+		"path: %s\n"
+		"pars: %s\n",
+		addr.proto, 
+		addr.domain, 
+		addr.port, 
+		addr.path, 
+		addr.pars
+	);
 
 	return 0;
 }
