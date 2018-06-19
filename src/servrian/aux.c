a20b90ef26b163236bdd074e3b5b9e9992f9bea6
blmayer <bleemayer@gmail.com>
2018-06-19 02:39:06 +0000

    Removed load_file function, reindented code.

--- src/servrian/aux.c
+++ src/servrian/aux.c
@@ -22,34 +22,11 @@ int file_size(char *path) {
 		return -1;
 	}
 	
-	fseek(page_file, 0, SEEK_END);	  /* Seek to the last byte of the file */
-	int page_size = ftell(page_file);   /* The SEEK position is the file size */
+	fseek(page_file, 0, SEEK_END);		/* Seek to the last byte */
+	int page_size = ftell(page_file);   	/* The position is the size */
 	fclose(page_file);
 	
 	return page_size;
-}
-
-char *load_file(char *path) {
-	
-	/* Open the file for reading */
-	FILE *page_file = fopen(path, "rb");
-	
-	if(page_file == NULL) {
-		perror("\t\tCannot open file");
-		return NULL;
-	}
-	
-	fseek(page_file, 0, SEEK_END);	  /* Seek to the last byte of the file */
-	int page_size = ftell(page_file);   /* The SEEK position is the file size */
-	rewind(page_file);				  /* Go back to the start of the file */
-	
-	/* Read it all in one operation and close */
-	char buff[page_size + 1];
-	fread(buff, page_size, sizeof(unsigned char), page_file);
-	fclose(page_file);				  /* Close the file */
-	buff[page_size] = 0;				/* Add the terminating zero */
-   
-	return strdup(buff);
 }
 
 int write_log(char *buff){
@@ -76,8 +53,8 @@ char *date_line(){
     cur_time = gmtime(&now);
     static char res_time[30]; 		/* Convert to local time format */
 
-    /* Date: Fri, 31 Dec 1999 23:59:59 GMT */
-    strftime(res_time, 32, "%a, %d %b %Y %X %Z", cur_time);
+    /* Date: Fri, 19 Feb 1992 08:53:17 GMT */
+    strftime(res_time, 30, "%a, %d %b %Y %X %Z", cur_time);
 
     return res_time;
 }
@@ -88,37 +65,37 @@ char *mime_type(char *path) {
 
 	/* Match the extension against some cases */
 	switch(strcmp(strrchr(path, '.'), ".bsog")) {
-		case 6:
-			mime = "text/html";
-			break;
-		
-		case 1:
-			mime = "text/css";
-			break;
-		
-		case 2:
-			mime = "application/javascript";
-			break;
-		
-		case 14:
-			mime = "image/png";
-			break;
-		
-		case 17:
-			mime = "image/svg+xml";
-			break;
-		
-		case 7:
-			mime = "image/x-icon";
-			break;
-			
-		case 21:
-			mime = "application/x-font-woff";
-			break;
-			
-		default:
-			mime = "application/octet-stream";
-			break;
+	case 6:
+		mime = "text/html";
+		break;
+	
+	case 1:
+		mime = "text/css";
+		break;
+	
+	case 2:
+		mime = "application/javascript";
+		break;
+	
+	case 14:
+		mime = "image/png";
+		break;
+	
+	case 17:
+		mime = "image/svg+xml";
+		break;
+	
+	case 7:
+		mime = "image/x-icon";
+		break;
+	
+	case 21:
+		mime = "application/x-font-woff";
+		break;
+	
+	default:
+		mime = "application/octet-stream";
+		break;
 	}
 	
 	/* Duplicate the token found to not get lost with function */
