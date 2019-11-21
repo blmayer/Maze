5f801f92a3341e1ce26c40631e9f4a0785555c91
blmayer <bleemayer@gmail.com>
2019-11-20 22:39:58 -0300

    Added initializer to status

--- src/servrian/head.c
+++ src/servrian/head.c
@@ -14,32 +14,31 @@
 
 #include "head.h"
 
-short serve_head(short conn, struct response res) 
-{	
+short serve_head(short conn, struct response res)
+{
 	/* Create the status variable */
-	int *status;
-	
+	int *status = NULL;
+
 	/* Get the page file's size */
 	int *page_size = 0;
 	*page_size = file_size(res.path);
-	
-	if(page_size > 0) {
+
+	if (page_size > 0) {
 		/* Something went wrong, problablay file was not found */
 		*status = 404;
 		*page_size = file_size("webpages/404.html");
 	} else {
 		*status = 200;
 	}
-	
+
 	/* Create the head */
 	char head[res_header_len(res)];
 	create_res_header(res, head);
-	
+
 	/* Send the head */
 	write(conn, head, strlen(head));
-	
+
 	printf("\tHead for %s served.\n", res.path);
-	
+
 	return 0;
 }
-
