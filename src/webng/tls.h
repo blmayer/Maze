63ec141861806f7bb169a067822579fccef9405f
blmayer <bleemayer@gmail.com>
2019-12-17 23:10:49 -0300

    Added support for 3 extensions
    - Changed message type to signed
    - Added include guards

--- src/webng/tls.h
+++ src/webng/tls.h
@@ -1,37 +1,49 @@
+#ifndef TLS_H
+#define TLS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "webng.h"
 
 /* A structure to hold SSL information needed to exchange messages */
 struct sslSession {
 	char ver[2];
+	unsigned char type;
 	unsigned char random[32];
 	unsigned char id[32];
 	unsigned short cypher;
-	unsigned char type;
 	char *proto;
 };
 
 /* Parses the ssl handshake */
-struct sslSession *do_ssl_handshake(int conn);
+int do_tls_handshake(int conn, struct sslSession *session);
 
 /* Parses the internal tls fragment */
-int parse_tls_handshake(unsigned char *fragment, struct sslSession *ssl_conn);
+int parse_handshake(int conn, char *fragment, struct sslSession *ssl_conn);
 
 /* Parse the tls client hello message */
-int parse_tls_client_hello(unsigned char *message, struct sslSession *ssl_conn);
+int parse_client_hello(char *message, struct sslSession *ssl_conn);
 
-int parse_extensions(unsigned char *msg, struct sslSession *sslConn);
+int send_server_hello(int conn, struct sslSession *ssl_conn);
 
-unsigned short parse_server_name_ext(unsigned char **msg);
+int parse_extensions(char *msg, struct sslSession *ssl_conn);
 
-unsigned short parse_supported_groups_ext(unsigned char **msg);
+unsigned short parse_server_name_ext(char **msg);
 
-unsigned short parse_ec_point_formats_ext(unsigned char **msg);
+unsigned short parse_supported_groups_ext(char **msg);
 
-unsigned short parse_signature_algorithms_ext(unsigned char **msg);
+unsigned short parse_ec_point_formats_ext(char **msg);
 
-unsigned short parse_use_srtp_ext(unsigned char **msg);
+unsigned short parse_signature_algorithms_ext(char **msg);
 
-unsigned short parse_proto_negotiation_ext(unsigned char **msg);
+unsigned short parse_use_srtp_ext(char **msg);
+
+unsigned short parse_proto_negotiation_ext(char **msg);
+
+unsigned short parse_encrypt_then_mac_ext(char **msg);
+
+unsigned short parse_extended_master_secret_ext(char **msg);
+
+unsigned short parse_session_ticket_ext(char **msg);
+
+#endif
