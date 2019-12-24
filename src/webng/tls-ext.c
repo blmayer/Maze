17745862b5a396c4b08cb3e0918c03fdc60a2bd0
blmayer <bleemayer@gmail.com>
2019-12-24 15:25:18 -0300

    Added unsigned mod to char and removed from short

--- src/webng/tls-ext.c
+++ src/webng/tls-ext.c
@@ -1,10 +1,11 @@
 #include "webng.h"
 #include "tls.h"
+#include "webng.h"
 
 /*
  * TODO: Use an int to keep track of *msg
  */
-int parse_extensions(char *msg, struct sslSession *ssl_conn)
+int parse_extensions(unsigned char *msg, struct tlsSession *session)
 {
 	/* Length of all extensions */
 	unsigned short exts_len = *msg++ << 8;
@@ -71,7 +72,7 @@ int parse_extensions(char *msg, struct sslSession *ssl_conn)
 	return 0;
 }
 
-unsigned short parse_server_name_ext(char **msg)
+short parse_server_name_ext(unsigned char **msg)
 {
 	/* Server name extension length */
 	unsigned short ext_len = *(*msg)++ << 8;
@@ -117,7 +118,7 @@ unsigned short parse_server_name_ext(char **msg)
 	return ext_len + 2;
 }
 
-unsigned short parse_supported_groups_ext(char **msg)
+short parse_supported_groups_ext(unsigned char **msg)
 {
 	/* Supported groups extension length */
 	unsigned short ext_len = *(*msg)++ << 8;
@@ -139,7 +140,7 @@ unsigned short parse_supported_groups_ext(char **msg)
 	return ext_len + 2;
 }
 
-unsigned short parse_ec_point_formats_ext(char **msg)
+short parse_ec_point_formats_ext(unsigned char **msg)
 {
 	/* EC point formats extension length */
 	unsigned short ext_len = *(*msg)++ << 8;
@@ -156,7 +157,7 @@ unsigned short parse_ec_point_formats_ext(char **msg)
 	return ext_len + 2;
 }
 
-unsigned short parse_signature_algorithms_ext(char **msg)
+short parse_signature_algorithms_ext(unsigned char **msg)
 {
 	/* Signature algorithms extension length */
 	unsigned short ext_len = *(*msg)++ << 8;
@@ -178,7 +179,7 @@ unsigned short parse_signature_algorithms_ext(char **msg)
 	return ext_len + 2;
 }
 
-unsigned short parse_use_srtp_ext(char **msg)
+short parse_use_srtp_ext(unsigned char **msg)
 {
 	/* SRTP Protection Profile */
 	unsigned short profile = **msg++ << 8;
@@ -203,7 +204,7 @@ unsigned short parse_use_srtp_ext(char **msg)
 	return 2 + profile + mki_len;
 }
 
-unsigned short parse_proto_negotiation_ext(char **msg)
+short parse_proto_negotiation_ext(unsigned char **msg)
 {
 	/* App-Layer protocol negotiation length */
 	unsigned short ext_len = *(*msg)++ << 8;
@@ -261,7 +262,7 @@ unsigned short parse_extended_master_secret_ext(char **msg)
 	return ext_len + 2;
 }
 
-unsigned short parse_session_ticket_ext(char **msg)
+short parse_session_ticket_ext(unsigned char **msg)
 {
 	/* Ticket extension length */
 	unsigned short ext_len = *(*msg)++ << 8;
